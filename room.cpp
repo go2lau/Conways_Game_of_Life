106eadb336304a012e8dca419c84fe289aeacd57
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-09 15:31:57 -0600

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,31 +1,36 @@
-#include <iostream>
 #include "room.h"
 using namespace std;
 
 Room::Room(){
     isAvailable=0;
     type= 0;
-    features="";
+    features={};
     price=0;
     number=0;
 }
 
-Room::Room(bool isAvailable, bool type, string features, float price, int number){
-    //The values in this method are all intended to be randomized, which is why they have temporarily remained the same. 
-    isAvailable=0;
-    type=0; 
-    features="";
-    price=0;
-    number=0;
+Room::Room(bool isAvailable_, bool type_, string features_[], float price_, int num_features){
+    isAvailable=isAvailable_;
+    type=type_; 
+    for (int i=0; i<num_features; i++){
+        features[i]=features_[i];
+    }
+    price=price_;
 }
 
-string Room::get_room_info(){
+vector <string> Room::get_room_info(){
     return features;
 }
 
 void Room::book(){
     if (isAvailable==1){
         isAvailable= !isAvailable;
+        
     }
 }
 
+void Room::cancel_booking(){
+    if (isAvailable==0){
+        isAvailable= !isAvailable;
+    }
+}
