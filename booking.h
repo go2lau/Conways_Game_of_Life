52a1c9e52851d9a139b0cef96415fa447d826eb0
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-09 15:28:14 -0600

    Update booking.h

--- booking.h
+++ booking.h
@@ -2,19 +2,18 @@
 #define BOOKING_H
 #include <string>
 #include <array>
-#include <vector>
-#include "room.h"
+#include "payment.h"
 using namespace std;
 class Booking{
     private:
         int booking_id;
-        vector <Room> rooms_booked;
+        Payment payment;
         array<int,3> check_in_date;
         array<int,3> check_out_date;
     public:
         Booking();
-        Booking(int val1, Room vector[], int array1[], int array2[], int val2);
-        void cancel_booking();
-        vector <Room> get_rooms_booked(); 
+        Booking(int val1, int array1[], int array2[]);
+        int get_id();
+        array<int,3> get_dates();
 };
 #endif
