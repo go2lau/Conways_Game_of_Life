ef9eae9cda0219e46e6dea4227c70b30eebaf96a
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-13 23:41:13 -0600

    Update guest.cpp

--- guest.cpp
+++ guest.cpp
@@ -2,29 +2,51 @@
 
 Guest::Guest(){
     name="";
-    contact_info={};
+    phone_number="";
+    email="";
     bookings_made={};
+    payments_made={};
 
 }
 
-Guest::Guest(string name_, string contact_info_[], Booking bookings_made_[],int num_bookings ){
+Guest::Guest(string name_, string phone_number_, string email_, Booking bookings_made_[], Payment payments_made_[], int num_bookings, int num_payments){
     name=name_;
-    for(int i=0; i<2; i++){
-        contact_info[i]=contact_info_[i];
-    }
+    
     for(int i=0; i<num_bookings; i++ ){
         bookings_made[i]=bookings_made_[i];
     }
+
+    phone_number=phone_number_;
+    email=email_;
+
+    for(int i=0; i<num_payments; i++){
+        payments_made[i]=payments_made_[i];
+    }
 }
 
-string Guest::get_name(){
-    return name;
+void Guest::set_bookings(vector<Booking> bookings_made_){
+
+    bookings_made= bookings_made_;
 }
 
-array<string,2> Guest::get_contact_info(){
-    return contact_info;
+void Guest::set_payments(vector<Payment> payments_made_){
+
+    payments_made=payments_made_;
+}
+
+string Guest::get_guest_info(){
+
+    string info= "Name: "+ name + "\nPhone number: "+ phone_number + "\nEmail: "+email;
+    return info;
 }
 
 vector <Booking> Guest::get_bookings(){
     return bookings_made;
 }
+
+vector <Payment> Guest::get_payments(){
+
+    return payments_made;
+}
+
+
