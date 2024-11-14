a4d842d726f9305ef08ed0d595770ee3b66c87eb
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-13 23:44:20 -0600

    Update room.h

--- room.h
+++ room.h
@@ -7,16 +7,19 @@ using namespace std;
 class Room{
     protected: 
         bool isAvailable;
-        bool type;
+        string type;
         vector<string> features;
         float price;
         int number;
     public: 
         Room();
-        Room(bool boolean, bool boolean2, string data[], float num , int val);
-        vector<string> get_room_info();
+        Room(bool boolean, string data2, string data[], float num , int val, int val2);
+        string get_type();
+        vector<string> get_features();
         void book();
         void cancel_booking();
+        float get_price();
+        int get_room_number();
 
 };
 #endif
