35f0e491f01d9a20b7c44fab5f005f3471e261a2
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-13 23:40:15 -0600

    Update booking.cpp

--- booking.cpp
+++ booking.cpp
@@ -5,7 +5,7 @@ Booking::Booking(){
     booking_id=0;
     check_in_date={};
     check_out_date={};
-
+    
 }
 
 Booking::Booking(int booking_id_, int check_in_date_[], int check_out_date_[]){
@@ -18,7 +18,22 @@ Booking::Booking(int booking_id_, int check_in_date_[], int check_out_date_[]){
 
 }
 
-int Booking::get_id(){
+void Booking::set_checkin(array<int,3> check_in_date_){
+
+    for (int i=0; i<3; i++){
+        check_in_date[i]=check_in_date_[i];
+    }
+}
+
+void Booking::set_checkout(array<int,3> check_out_date_){
+
+    for (int i=0; i<3; i++){
+        check_out_date[i]=check_out_date_[i];
+    }
+}
+
+
+int Booking::get_booking_id(){
     return booking_id;
 }
 
@@ -26,3 +41,6 @@ array<int,3> Booking::get_dates(){
     return check_in_date;
     return check_out_date;
 }
+
+
+
