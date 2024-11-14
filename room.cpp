65be8ca84994f35ff4d92c94e824d0afb79a9165
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-13 23:43:51 -0600

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 Room::Room(){
     isAvailable=0;
-    type= 0;
+    type= "";
     features={};
     price=0;
     number=0;
 }
 
-Room::Room(bool isAvailable_, bool type_, string features_[], float price_, int num_features){
+Room::Room(bool isAvailable_, string type_, string features_[], float price_, int number_, int num_features){
     isAvailable=isAvailable_;
     type=type_; 
     for (int i=0; i<num_features; i++){
         features[i]=features_[i];
     }
     price=price_;
+    number=number_;
 }
 
-vector <string> Room::get_room_info(){
+vector <string> Room::get_features(){
     return features;
 }
 
@@ -33,4 +34,18 @@ void Room::cancel_booking(){
     if (isAvailable==0){
         isAvailable= !isAvailable;
     }
+}
+
+float Room::get_price(){
+
+    return price;
+}
+
+int Room::get_room_number(){
+
+    return number;
+}
+
+string Room::get_type(){
+    return type;
 }
