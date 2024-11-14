ef62840e8a4fb5cd5f1d96f74621518db277ed7d
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-13 23:40:43 -0600

    Update booking.h

--- booking.h
+++ booking.h
@@ -7,13 +7,14 @@ using namespace std;
 class Booking{
     private:
         int booking_id;
-        Payment payment;
         array<int,3> check_in_date;
         array<int,3> check_out_date;
     public:
         Booking();
         Booking(int val1, int array1[], int array2[]);
-        int get_id();
+        void set_checkin(array<int,3>);
+        void set_checkout(array<int,3>);
         array<int,3> get_dates();
+        int get_booking_id();
 };
 #endif
