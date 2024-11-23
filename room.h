ea9b2cc99c6e553ac178d48f30fbd0d3f65f9f52
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-23 15:07:32 -0600

    Update room.h

--- room.h
+++ room.h
@@ -1,24 +1,24 @@
-#ifndef ROOM_H
+#ifndef ROOM_H 
 #define ROOM_H
-#include <string>
+#include <string> // Include string library for using strings
 using namespace std;
 class Room{
     protected: 
-        bool isAvailable;
+        bool isAvailable;// Boolean to store whether the room is available (1 for available, 0 for not available)
         string type;
         string features;
         float price;
         int number;
     public: 
-        Room();
+        Room(); 
         Room(bool boolean, string data2, string data3, float num , int val);
         string get_type();
         string get_features();
         float get_price();
         int get_room_number();
         string get_availability();
+        // Setter function for room availability: Sets the room's availability status
         void set_availability(bool);
 
 };
-#endif
 #endif
