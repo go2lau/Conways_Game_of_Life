edb86b8c061776e5d4b0cb8cbe7c51d89ce84a95
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-23 14:18:48 -0600

    Update room.h

--- room.h
+++ room.h
@@ -14,13 +14,11 @@ class Room{
         Room(bool boolean, string data2, string data3, float num , int val);
         string get_type();
         string get_features();
-        string get_room_info();
         float get_price();
         int get_room_number();
         string get_availability();
         void set_availability(bool);
-        void set_features(string);
-        void set_price(float);
 
 };
+#endif
 #endif
