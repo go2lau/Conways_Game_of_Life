d3d948a0504446266aeddb4023edc7f38c52ee38
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-23 15:04:59 -0600

    Update guest.h

--- guest.h
+++ guest.h
@@ -1,26 +1,27 @@
 #ifndef GUEST_H
 #define GUEST_H
-#include <string>
-#include "booking.h"
-#include "room.h"
-#include "suite.h"
+#include <string> // Include string library for using strings
+#include "booking.h" // Include the Booking class header
+#include "room.h" // Include the Room class header
+#include "suite.h"  // Include the Suite class header
 using namespace std; 
 
 class Guest{
 
     private:
-        string name;
+        string name; 
         string phone_number;
         string email; 
-        Booking booking;
-        Room room;
-        Suite suite;
+        Booking booking; // Booking object to store the guest's booking details (aggregation relationship)
+        Room room; // Room object to store the guest's booking details (aggregation relationship)
+        Suite suite; // Suite object to store the guest's booking details (aggregation relationship)
     public:
         Guest();
         Guest(string data, string data2, string data3, Booking booking, Room room, Suite suite);
-        string book_room(Room, Booking);
+        // Function to book a room: Takes a Room and Booking object, returns a message indicating if the booking was successful
+        string book_room(Room, Booking); 
+         // Function to book a suite: Takes a Suite and Booking object, returns a message indicating if the booking was successful
         string book_suite(Suite, Booking);
-        string get_guest_info();
         string get_email();
         string get_phone();
         string get_guest_name();
