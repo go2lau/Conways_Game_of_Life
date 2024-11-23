d45be0fb2e56338799e8a6a4b7fcc41a8e7c7715
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-23 15:03:44 -0600

    Update booking.h

--- booking.h
+++ booking.h
@@ -1,18 +1,20 @@
 #ifndef BOOKING_H
 #define BOOKING_H
-#include <string>
 #include <array>
 using namespace std;
 class Booking{
     private:
-        int booking_id;
-        array<int,3> check_in_date;
-        array<int,3> check_out_date;
+        int booking_id;// Integer to store the booking ID
+        array<int,3> check_in_date; // Array to store the check-in date (3 elements: day, month, year)
+        array<int,3> check_out_date; //Same as check in
     public:
         Booking();
         Booking(int val1, array<int,3>, array<int,3>);
+        // Getter function to retrieve the check-in date
         array<int,3> get_checkin();
+        // Getter function to retrieve the check-out date
         array<int,3> get_checkout();
+        // Getter function to retrieve the booking ID
         int get_booking_id();
 
 };
