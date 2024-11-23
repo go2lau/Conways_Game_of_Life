7e9f06e25b3ca88b421a2a06963e0e2e5d2d109f
Laura Thomas <peluchethomasgodos@gmail.com>
2024-11-23 15:03:12 -0600

    Update booking.cpp

--- booking.cpp
+++ booking.cpp
@@ -1,6 +1,7 @@
-#include "booking.h"
+#include "booking.h" // Include the header file for the Booking class
 using namespace std;
 
+// Constructor with no parameters: Initializes the booking_id to 0 and empty check-in and check-out dates
 Booking::Booking(){
     booking_id=0;
     check_in_date={};
@@ -8,6 +9,7 @@ Booking::Booking(){
     
 }
 
+// Constructor with parameters to initialize the booking_id, check_in_date, and check_out_date
 Booking::Booking(int booking_id_, array<int,3> check_in_date_, array<int,3> check_out_date_){
     booking_id=booking_id_;
     for (int i=0; i<3; i++){
@@ -18,19 +20,23 @@ Booking::Booking(int booking_id_, array<int,3> check_in_date_, array<int,3> chec
 
 }
 
-
-
+// Getter function for the booking_id. Returns the booking ID of the current booking.
 int Booking::get_booking_id(){
     return booking_id;
 }
 
+// Getter function for the check-in date. Returns the check_in_date array.
 array<int,3> Booking::get_checkin(){
     return check_in_date;
 }
 
+// Getter function for the check-out date. Returns the check_out_date array.
 array<int,3> Booking::get_checkout(){
     return check_out_date;
 }
+
+
+
 
 
 
